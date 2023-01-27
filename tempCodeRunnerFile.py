698e248799cfa6c9de93ff83ec8496fb86adcffa
Divyam Jain <jaindivyam111@gmail.com>
2023-01-27 21:18:44 +0530

    updates and bug fix

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
- # if(self.x<0):
-        #     self.dirx=1
-        # if(self.y<0):
-        #     self.diry=1
-        # if(self.x>500):
-        #     self.dirx=-1
-        # if(self.y>450):
-        #     self.diry=-1
+self.posx=self.dirx*self.magnitude+self.posx
+        self.posy=self.diry*self.magnitude+self.posy
